eb038ec9104b77762e6f3ba2a926aa82994b8e69
tugumeandree <andrewtugume2@gmail.com>
2025-12-05 18:39:01 +0300

    added more hymns

--- data/hymns.ts
+++ data/hymns.ts
@@ -947,7 +947,7 @@ export const hymnsDatabase: Hymn[] = [
     number: 20,
     title: "Kangume Niiwe",
     language: "Runyankore",
-    category: "Faith",
+    category: "Worship",
     lyrics: [
       {
         type: "chorus",
